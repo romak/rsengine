d18657a54dd01cbe73afae0eb50d3651aebce702
romak <romankalinchuk@gmail.com>
2013-05-28 21:19:31 +0300

    rs

--- code/inc/kernel/platform.h
+++ code/inc/kernel/platform.h
@@ -1,21 +1,21 @@
 /*
- * Copyright (C) 2009-2011 - Roman Kalynchuk
- *
- * This file is part of REngine.
- *
- * REngine is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * REngine is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with REngine.  If not, see <http://www.gnu.org/licenses/>.
- */
+* Copyright (C) 2009-2013 - Roman Kalynchuk
+*
+* This file is part of rsengine.
+*
+* REngine is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* REngine is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with rsengine.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef __PLATFORM_H__
 #define __PLATFORM_H__
